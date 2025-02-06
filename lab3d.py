7b2920d72f4f0e6f5a6492ca00c61f99886f66f9
Priyanshu Sargra <psargra@myseneca.ca>
2025-02-06 16:21:36 -0500

    this is lab3d

---
+++ lab3d.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Lab 3 Inv 2 - Free Disk Space'''
+# Author ID:Psargra
+
+import subprocess
+
+def free_space():
+    p = subprocess.Popen("df -h | grep '/$' | awk '{print $4}'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+    output = p.communicate()
+
+    stdout = output[0].decode('utf-8').strip()
+    return stdout
+
+if __name__ == '__main__':
+     print(free_space())
